0840c660c78d854e1fc4c6a0a79beef1024f68e3
seokju-na <seokju.me@gmail.com>
2019-05-26 01:28:50 +0900

    Remove useless files

--- services/backend/src/domain/shared/utils/generateUniqueId.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var generateUUId = require("uuid/v4");
-function generateUniqueId() {
-    return generateUUId();
-}
-exports["default"] = generateUniqueId;
