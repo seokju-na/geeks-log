f665406ea3b1b55a48e1dbecb32dec75d807fb7c
seokju-na <seokju.me@gmail.com>
2019-08-03 20:06:52 +0900

    Add 'common' supports

--- libs/package-manager/src/models/PackageInfo.ts
+++ libs/package-manager/src/models/PackageInfo.ts
@@ -1,6 +1,6 @@
 import { ROOT_PACKAGE } from '../constants';
 
-export type PackageType = 'library' | 'service' | '(root)';
+export type PackageType = 'library' | 'common' | 'service' | '(root)';
 
 export default interface PackageInfo {
   readonly pkgName: string;
@@ -10,11 +10,13 @@ export default interface PackageInfo {
   readonly type: PackageType;
 }
 
-export const packagePathMatches = /^((libs|services)\/(.+?))\//g;
+export const packagePathMatches = /^((libs|common|services)\/(.+?))\//g;
 
 export function getPackageType(pkg: string): PackageType {
   if (pkg.startsWith('libs')) {
     return 'library';
+  } else if (pkg.startsWith('common')) {
+    return 'common';
   } else if (pkg.startsWith('services')) {
     return 'service';
   } else {
